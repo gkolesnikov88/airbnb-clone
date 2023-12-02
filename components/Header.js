8029b6325c950295747a863e7fcedd3e35db2b88
George Kolesnikov <gkolesnikov88@gmail.com>
2023-12-02 17:37:27 +0300

    Header. Add right side with icons

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
-import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import { 
+    MagnifyingGlassIcon,
+    GlobeAltIcon,
+    Bars3Icon, 
+    UserCircleIcon
+} from '@heroicons/react/24/solid'
 
 function Header() {
   return (
@@ -30,7 +35,14 @@ function Header() {
         </div>
         
         {/* right */}
-        <div></div>
+        <div className='flex items-center justify-end space-x-4 text-gray-500'>
+            <p className='hidden md:inline cursor-pointer'>Become a host</p>
+            <GlobeAltIcon className='h-6 cursor-pointer'/>
+            <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
+                <Bars3Icon className='h-6'/>
+                <UserCircleIcon className='h-6'/>
+            </div>
+        </div>
     </header>
   )
 }
