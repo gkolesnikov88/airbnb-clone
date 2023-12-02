e3f383e6142697ac9746f1635ef452fafb86fce2
George Kolesnikov <gkolesnikov88@gmail.com>
2023-12-02 07:41:03 +0300

    Header. Add left part with icon

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Header() {
   return (
-    <header>
+    <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md
+        p-5 md:px-10 '>
         {/* left */}
-        <div></div>
+        <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+            <Image 
+                className='object-contain'
+                src={'https://upload.wikimedia.org/wikipedia/commons/6/69/Airbnb_Logo_B%C3%A9lo.svg'}
+                layout='fill'
+                objectPosition='left'
+            />
+        </div>
         
         {/* middle */}
         <div></div>
